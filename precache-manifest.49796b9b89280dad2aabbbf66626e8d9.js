a73cf6c7a23960df66e6571ae759cf293ab0aa29
Kharanshu <dewankharanshu@gmail.com>
2020-06-07 21:26:42 +0530

    Updates

---
+++ precache-manifest.49796b9b89280dad2aabbbf66626e8d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9317895254c5f955ce2619cfb8cfa7",
+    "url": "/elocalProject/index.html"
+  },
+  {
+    "revision": "16c932bbf6a51e8b12c7",
+    "url": "/elocalProject/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "156b59bfb97cf0aecac8",
+    "url": "/elocalProject/static/css/main.dd9296a9.chunk.css"
+  },
+  {
+    "revision": "16c932bbf6a51e8b12c7",
+    "url": "/elocalProject/static/js/2.7f5006c0.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/elocalProject/static/js/2.7f5006c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "156b59bfb97cf0aecac8",
+    "url": "/elocalProject/static/js/main.aa348ddd.chunk.js"
+  },
+  {
+    "revision": "df1bd192a1be959ea363",
+    "url": "/elocalProject/static/js/runtime-main.02369c68.js"
+  },
+  {
+    "revision": "23f65e758c4ed49843520fb816532d4e",
+    "url": "/elocalProject/static/media/1+7(2).23f65e75.jpg"
+  },
+  {
+    "revision": "fec6c51cf4c12d068b7e25e05fe90e41",
+    "url": "/elocalProject/static/media/telecom.fec6c51c.jpg"
+  }
+]);
